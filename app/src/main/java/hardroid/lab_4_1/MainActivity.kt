3a32cb97540f7e624b208d540d4681014d8b65dd
ArtemBatkov <artembatkov2015@gmail.com>
2022-10-19 01:53:43 +0300

    Final:
    Added toggle's style and buttons' styles

---
+++ app/src/main/java/hardroid/lab_4_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package hardroid.lab_4_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
