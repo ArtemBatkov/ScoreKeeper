2aded66855b5e23e3db89fea08a1386bc5387182
ArtemBatkov <artembatkov2015@gmail.com>
2022-10-19 02:31:51 +0300

    Lab 5
    Step 1
    
    Bound all necessary views

--- app/src/main/java/hardroid/lab_4_1/MainActivity.kt
+++ app/src/main/java/hardroid/lab_4_1/MainActivity.kt
@@ -2,10 +2,36 @@ package hardroid.lab_4_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var Score_A:TextView = TODO()
+    private var Score_B:TextView = TODO()
+    private var Switch:Switch = TODO()
+    private var Plus: Button = TODO()
+    private var Minus:Button = TODO()
+    private var RG: RadioGroup = TODO()
+    private var Score = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Score_A = findViewById<TextView>(R.id.a_score)
+        Score_B = findViewById<TextView>(R.id.b_score)
+        Switch = findViewById<Switch>(R.id.toggle)
+        Plus = findViewById<Button>(R.id.plus)
+        Minus =  findViewById<Button>(R.id.minus)
+        RG = findViewById<RadioGroup>(R.id.RG)
+
+
+
+
+
+
     }
+
 }
