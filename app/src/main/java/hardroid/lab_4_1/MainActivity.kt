26ee3d9583e914dd3da2719d8f0ca3606cd1679b
ArtemBatkov <artembatkov2015@gmail.com>
2022-10-19 04:21:29 +0300

    Lab 5
    Step 2
    
    Added a lottie file to the proj
    Made some changes of the styles
    Add logic for buttons
    Created some new layouts and kt file
    Done the lab 5

--- app/src/main/java/hardroid/lab_4_1/MainActivity.kt
+++ app/src/main/java/hardroid/lab_4_1/MainActivity.kt
@@ -2,34 +2,65 @@ package hardroid.lab_4_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Switch
 import android.widget.TextView
 
+/*
+THIS FILE IS SCOREKEEPER LOGIC
+ */
+
 class MainActivity : AppCompatActivity() {
-    private var Score_A:TextView = TODO()
-    private var Score_B:TextView = TODO()
-    private var Switch:Switch = TODO()
-    private var Plus: Button = TODO()
-    private var Minus:Button = TODO()
-    private var RG: RadioGroup = TODO()
-    private var Score = 0;
+
+     private var Score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Score_A = findViewById<TextView>(R.id.a_score)
-        Score_B = findViewById<TextView>(R.id.b_score)
-        Switch = findViewById<Switch>(R.id.toggle)
-        Plus = findViewById<Button>(R.id.plus)
-        Minus =  findViewById<Button>(R.id.minus)
-        RG = findViewById<RadioGroup>(R.id.RG)
+        //Initialize ALL Views
+        var Score_A: TextView = findViewById<TextView>(R.id.a_score)
+        var Score_B: TextView = findViewById<TextView>(R.id.b_score)
+        var Switch: Switch = findViewById<Switch>(R.id.toggle)
+        var Plus: View =  findViewById(R.id.plus)
+        var Minus: Button = findViewById<Button>(R.id.minus)
+        var RG: RadioGroup = findViewById<RadioGroup>(R.id.RG)
 
 
+        //Plus reaction
+        Plus.setOnClickListener{
+            var RadioID = findViewById<RadioButton>(RG.checkedRadioButtonId) // get the pressed radio button ID and find the view
+            var incrementedBy = RadioID.text.toString().toInt() // translate from string to Int
+            if(!Switch.isChecked){//if it isn't toggled
+                Score = Score_A.text.toString().toInt()//get text and convert to Int
+                Score_A.text = if (Score+incrementedBy >= 20) "20" else "${Score+incrementedBy}" // define what we are going to add to View
+            }
+            else{//all staff does the same things
+                Score = Score_B.text.toString().toInt()
+                Score_B.text = if (Score+incrementedBy >= 20) "20" else "${Score+incrementedBy}"
+            }
+        }
 
+        Minus.setOnClickListener{
+            var RadioID = findViewById<RadioButton>(RG.checkedRadioButtonId)
+            var dicrementedBy = RadioID.text.toString().toInt()
+            if(!Switch.isChecked){
+                Score = Score_A.text.toString().toInt()
+                Score_A.text = if (Score-dicrementedBy < 0) "0" else "${Score-dicrementedBy}"
+            }
+            else{
+                Score = Score_B.text.toString().toInt()
+                Score_B.text = if (Score-dicrementedBy < 0) "0" else "${Score-dicrementedBy}"
+            }
+        }
 
+        //changes the text in toggle
+        Switch.setOnClickListener {
+            Switch.text = if(!Switch.isChecked)  getString(R.string.team_a) else  getString(R.string.team_b)
+        }
 
 
     }
